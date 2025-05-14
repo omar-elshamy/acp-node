22bb1b0525f82d5a73419946bc20f1de4308c465
Ang Weoy Yang <weoyyang00@gmail.com>
2025-05-14 12:46:47 +0800

    feat: add raw examples

---
+++ examples/raw/external_evaluation/buyer.ts
@@ -0,0 +1,46 @@
+// TODO: Point the imports to acp-node after publishing
+
+import AcpClient from "../../../src/acpClient";
+import AcpContractClient, { AcpJobPhases } from "../../../src/acpContractClient";
+import AcpJob from "../../../src/acpJob";
+import { baseSepoliaAcpConfig } from "../../../src";
+import {
+    BUYER_WALLET_ADDRESS,
+    EVALUATOR_WALLET_ADDRESS,
+    SELLER_WALLET_ADDRESS,
+    WHITELISTED_WALLET_ENTITY_ID,
+    WHITELISTED_WALLET_PRIVATE_KEY
+} from "./env";
+
+async function buyer() {
+    const acpClient = new AcpClient({
+        acpContractClient: await AcpContractClient.build(
+            WHITELISTED_WALLET_PRIVATE_KEY,
+            WHITELISTED_WALLET_ENTITY_ID,
+            BUYER_WALLET_ADDRESS,
+            baseSepoliaAcpConfig
+        ),
+        onNewTask: async (job: AcpJob) => {
+            if (
+                job.phase === AcpJobPhases.NEGOTIATION &&
+                job.memos.find((m) => m.nextPhase === AcpJobPhases.TRANSACTION)
+            ) {
+                console.log("Paying job", job);
+                await job.pay(1);
+                console.log(`Job ${job.id} paid`);
+            } else if (job.phase === AcpJobPhases.COMPLETED) {
+                console.log(`Job ${job.id} completed`);
+            }
+        },
+    });
+
+    const jobId = await acpClient.initiateJob(
+        SELLER_WALLET_ADDRESS,
+        "Meme generator",
+        undefined,
+        EVALUATOR_WALLET_ADDRESS
+    );
+    console.log(`Job ${jobId} initiated`);
+}
+
+buyer();
---
+++ examples/raw/self_evaluation/seller.ts
@@ -0,0 +1,46 @@
+// TODO: Point the imports to acp-node after publishing
+
+import AcpClient from "../../../src/acpClient";
+import AcpContractClient, { AcpJobPhases } from "../../../src/acpContractClient";
+import AcpJob from "../../../src/acpJob";
+import { baseSepoliaAcpConfig } from "../../../src";
+import {
+    SELLER_WALLET_ADDRESS,
+    WHITELISTED_WALLET_ENTITY_ID,
+    WHITELISTED_WALLET_PRIVATE_KEY
+} from "./env";
+
+async function seller() {
+    new AcpClient({
+        acpContractClient: await AcpContractClient.build(
+            WHITELISTED_WALLET_PRIVATE_KEY,
+            WHITELISTED_WALLET_ENTITY_ID,
+            SELLER_WALLET_ADDRESS,
+            baseSepoliaAcpConfig
+        ),
+        onNewTask: async (job: AcpJob) => {
+            if (
+                job.phase === AcpJobPhases.REQUEST &&
+                job.memos.find((m) => m.nextPhase === AcpJobPhases.NEGOTIATION)
+            ) {
+                console.log("Responding to job", job);
+                await job.respond(true);
+                console.log(`Job ${job.id} responded`);
+            } else if (
+                job.phase === AcpJobPhases.TRANSACTION &&
+                job.memos.find((m) => m.nextPhase === AcpJobPhases.EVALUATION)
+            ) {
+                console.log("Delivering job", job);
+                await job.deliver(
+                    JSON.stringify({
+                        type: "url",
+                        value: "https://example.com",
+                    })
+                );
+                console.log(`Job ${job.id} delivered`);
+            }
+        },
+    });
+}
+
+seller();
