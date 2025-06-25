c243c362bb472c7745eb796fa3736cdc259d3d17
Zuhwa <zuhwa@virtuals.io>
2025-06-25 14:47:59 +0800

    bump to 0.1.0-beta.6

--- src/configs.ts
+++ src/configs.ts
@@ -19,8 +19,8 @@ const baseSepoliaAcpConfig: AcpContractConfig = {
 
 const baseAcpConfig: AcpContractConfig = {
   chain: base,
-  contractAddress: "0x2422c1c43451Eb69Ff49dfD39c4Dc8C5230fA1e6",
-  virtualsTokenAddress: "0xbfAB80ccc15DF6fb7185f9498d6039317331846a",
+  contractAddress: "0x6a1FE26D54ab0d3E1e3168f2e0c0cDa5cC0A0A4A",
+  virtualsTokenAddress: "0x0b3e328455c4059EEb9e3f84b5543F74E24e7E1b",
   alchemyRpcUrl: "https://alchemy-proxy-prod.virtuals.io/api/proxy/rpc",
   acpUrl: "https://acpx.virtuals.io",
 };
